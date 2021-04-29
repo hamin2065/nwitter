b77213fa919a4d26075b547ef21c23a9b8a25d02
구하민 <hamin2065@gmail.com>
2021-04-29 23:50:51 +0900

    auth.js - email, password

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,18 @@
-import AppRouter from "./Router";
+import React, {useState, useEffect} from "react";
+import AppRouter from "components/Router";
+import {authService} from "fbase";
 
 function App() {
-  return <AppRouter/>;
+  console.log(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  return(
+    <>
+      <AppRouter isLoggedIn={isLoggedIn}/>
+      <footer>&copy;  {new Date().getFullYear()}Nwitter</footer>
+    
+    
+    </>
+
+  ); 
 }
 export default App;
