2b5c912e316bc2c0a81380f628ccb71a9541070c
구하민 <hamin2065@gmail.com>
2021-05-01 00:49:24 +0900

    Nweeting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import { dbService } from "fbase";
+import Nweet from "components/Nweet";
 
 const Home = ({userObj}) =>{
     console.log(userObj);
@@ -37,9 +38,7 @@ const Home = ({userObj}) =>{
     </form>
     <div>
         {nweets.map((nweet) => 
-        <div key = {nweet.id}>
-            <h4>{nweet.text}</h4>
-        </div>)}
+        <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid}/>)}
     </div>
 </div>
     );
